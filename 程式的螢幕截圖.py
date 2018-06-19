cbcd85546f973a448d43c2714492ed63424250fc
JNturtle <31470045+JNturtle@users.noreply.github.com>
2018-06-19 20:25:55 +0800

    Add files via upload

---
+++ 程式的螢幕截圖.py
@@ -0,0 +1,88 @@
+from PIL import ImageGrab
+import numpy as np
+import cv2
+import win32gui
+import win32con
+import win32ui
+
+import re
+from time import sleep
+
+'''
+製作者：JN
+參考網址
+https://www.codeproject.com/Articles/20651/Capturing-Minimized-Window-A-Kid-s-Trick
+https://www.programcreek.com/python/example/62809/win32ui.CreateBitmap
+'''
+
+
+def FindWindow_bySearch(pattern):
+    window_list = []
+    win32gui.EnumWindows(lambda hWnd, param: param.append(hWnd), window_list)
+    for each in window_list:
+        if re.search(pattern, win32gui.GetWindowText(each)) is not None:
+            return each
+
+def getWindow_W_H(hwnd):
+    # 取得目標視窗的大小
+    left, top, right, bot = win32gui.GetWindowRect(hwnd)
+    width = right - left + 1
+    height = bot - top + 1
+    return (left, top, width, height)
+
+def getWindow_Img(hwnd):
+    # 將 hwnd 換成 WindowLong
+    s = win32gui.GetWindowLong(hwnd,win32con.GWL_EXSTYLE)
+    win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, s|win32con.WS_EX_LAYERED)
+    # 判斷視窗是否最小化
+    show = win32gui.IsIconic(hwnd)
+    # 將視窗圖層屬性改變成透明    
+    # 還原視窗並拉到最前方
+    # 取消最大小化動畫
+    # 取得視窗寬高
+    if show == 1: 
+        win32gui.SystemParametersInfo(win32con.SPI_SETANIMATION, 0)
+        win32gui.SetLayeredWindowAttributes(hwnd, 0, 0, win32con.LWA_ALPHA)
+        win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)    
+        x, y, width, height = getWindow_W_H(hwnd)        
+    # 創造輸出圖層
+    hwindc = win32gui.GetWindowDC(hwnd)
+    srcdc = win32ui.CreateDCFromHandle(hwindc)
+    memdc = srcdc.CreateCompatibleDC()
+    bmp = win32ui.CreateBitmap()
+    # 取得視窗寬高
+    x, y, width, height = getWindow_W_H(hwnd)
+    # 如果視窗最小化，則移到Z軸最下方
+    if show == 1: win32gui.SetWindowPos(hwnd, win32con.HWND_BOTTOM, x, y, width, height, win32con.SWP_NOACTIVATE)
+    # 複製目標圖層，貼上到 bmp
+    bmp.CreateCompatibleBitmap(srcdc, width, height)
+    memdc.SelectObject(bmp)
+    memdc.BitBlt((0 , 0), (width, height), srcdc, (0, 0), win32con.SRCCOPY)
+    # 將 bitmap 轉換成 np
+    signedIntsArray = bmp.GetBitmapBits(True)
+    img = np.fromstring(signedIntsArray, dtype='uint8')
+    img.shape = (height,width,4)
+    # 釋放device content
+    srcdc.DeleteDC()
+    memdc.DeleteDC()
+    win32gui.ReleaseDC(hwnd, hwindc)
+    win32gui.DeleteObject(bmp.GetHandle())
+    # 還原目標屬性
+    if show == 1 :
+        win32gui.SetLayeredWindowAttributes(hwnd, 0, 255, win32con.LWA_ALPHA)
+        win32gui.SystemParametersInfo(win32con.SPI_SETANIMATION, 1)
+    # 回傳圖片
+    return img
+
+hwnd = FindWindow_bySearch("小畫家")
+print( win32gui.GetParent(hwnd) )
+
+while True:
+    sleep(0.03)
+    frame = getWindow_Img(hwnd)
+    cv2.imshow("screen box", frame)
+    k = cv2.waitKey(30)&0xFF #64bits! need a mask
+    if k ==27:
+        cv2.destroyAllWindows()
+        break
+
